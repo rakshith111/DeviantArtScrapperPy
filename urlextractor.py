8be2198b61130e99c7025b174755b3d50325ae12
rakshith111 <36219488+rakshith111@users.noreply.github.com>
2022-03-23 02:11:52 +0530

    Now with cookies

--- urlextractor.py
+++ urlextractor.py
@@ -1,16 +1,18 @@
 from time import sleep
 import requests
 from lxml import html
-def urlextractor(url):
+#from random import randint
+def urlextractor(url,cookie):
     '''
-    accepts a deviant url , searches for the next page cursor , and returns the cursor part 
+    accepts a deviant url along with its parent cookie, searches for the next page cursor , and returns the cursor part 
     if no next page  or any other error returns 0
     '''
     print("FROM URL EX "+url)
     try:
-        page = requests.get(url)
-        print(page)
-        open("asd.html",'wb').write(page.content)  
+        page = requests.get(url,cookies=cookie)
+        #nu=randint(0,10)
+        #print(f"writing {nu}")
+        #open(f"asd{nu}.html",'wb').write(page.content)  
         sleep(0.5)
         pagedata=html.fromstring(page.content)
         nextpage=pagedata.xpath('//*[text()="Next"]//@href')
