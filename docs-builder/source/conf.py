928d6b228d3acef6743f1336f6d795482dd54c70
rakshith111 <rakshithbabu111@gmail.com>
2022-12-02 00:46:10 +0530

    Update conf

--- docs-builder/source/conf.py
+++ docs-builder/source/conf.py
@@ -1,10 +1,3 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 import sys
 import os
@@ -14,28 +7,26 @@ copyright = '2022, rakshith111'
 author = 'rakshith111'
 release = '0.0.1'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-
-templates_path = ['_templates']
-exclude_patterns = []
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
 
 sys.path.insert(0, os.path.abspath('../../src'))
 
+templates_path = ['_templates']
+html_static_path = ['_static']
+
+exclude_patterns = []
+# add mock imports since we don't have the dependencies installed when building docs and we have imported modules in the same folder
+autodoc_mock_imports = ["urlextractor"]
+
+
+
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode',
+              'myst_parser']
 
 html_theme = 'furo'
 pygments_style = "sphinx"
 pygments_dark_style = "monokai"
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode',
-              'myst_parser']
+
 autoclass_content = 'both'  # add __init__ to the list of functions to document
 
 # To generate new files in the source folder
