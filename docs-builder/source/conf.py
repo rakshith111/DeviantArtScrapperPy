e373cba0f5957d7387ba1d57d4a86a0cf5e5541b
rakshith111 <rakshithbabu111@gmail.com>
2022-12-01 22:09:52 +0530

    Update markdown files

--- docs-builder/source/conf.py
+++ docs-builder/source/conf.py
@@ -8,8 +8,7 @@
 
 import sys
 import os
-import glob
-import subprocess
+
 project = 'DeviantArtScrapper'
 copyright = '2022, rakshith111'
 author = 'rakshith111'
@@ -19,10 +18,8 @@ release = '0.0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 
-
 templates_path = ['_templates']
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -39,21 +36,9 @@ pygments_style = "sphinx"
 pygments_dark_style = "monokai"
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode',
               'myst_parser']
-autoclass_content = 'both' #add __init__ to the list of functions to document
+autoclass_content = 'both'  # add __init__ to the list of functions to document
 
+# To generate new files in the source folder
+# run the command: sphinx-apidoc -o docs-builder/source src
+# usage: sphinx-apidoc [OPTIONS] -o <OUTPUT_PATH> <MODULE_PATH> [EXCLUDE_PATTERN, ...]
 
-def setup(app):
-    """Run some additional steps after the Sphinx builder is initialized"""
-
-    app.connect('builder-inited', convert)
-
-# Creates backup and converts all .rst files to .md
-
-
-def convert(app):
-    for file in (glob.glob(os.path.join(os.getcwd()+'\docs\source\*.rst'))):
-        print("Converting", file)
-
-        subprocess.run(["copy", f"{file}",f"{file}.bak"], shell=True)
-        subprocess.run(["rst2myst", "convert",f"{file}"], shell=True)
-        subprocess.run(["del", f"{file}", ], shell=True)
