0f23e5f98c30e2851aebec9eb0369aa227898602
rakshith111 <rakshithbabu111@gmail.com>
2022-12-01 21:38:22 +0530

    Add new method - selenuim version next cursor finder update comments

--- src/libs/urlextractor.py
+++ src/libs/urlextractor.py
@@ -2,20 +2,26 @@ from time import sleep
 import requests
 from lxml import html
 #from random import randint
-from requests import cookies
 
 
-def nextcursor(baseurl, cookie):
+def nextcursor_requests(baseurl: str, cookie) -> str:
     '''
-    :param baseurl: str - Deviant art page url
-    :param cookie: cookie - cookie of the parent page
-    :return: str - next page cursor 
+    :param str baseurl:  Deviant art page url
+    :param cookie cookie:  cookie of the parent page
+    :return:  next page cursor 
+    :rtype: str
     :example: "/tag/steamprofile?cursor=MTQwYWI2MjA9NCY1OTBhY2FkMD03MiZkMTc0YjZiYz1OJTJGQQ"
 
+    .. deprecated:: 0.0.1
+        Use :func:`deviantartapi.nextcursor_selenium` instead
+        outdated since Deviantart changed their website and introduced login system
+        Requests version doesn't render js
+
+    | Made for requests version
     | Accepts a deviant url along with its parent cookie, searches for the next page cursor , and returns the cursor part 
     | if no next page  or any other error returns 0
 
-    
+
     '''
     try:
         page = requests.get(baseurl, cookies=cookie)
@@ -28,15 +34,40 @@ def nextcursor(baseurl, cookie):
         return 0
 
 
-def remove_filter(steamurl):
+def nextcursor_selenium(page_data: str) -> str:
+    '''
+
+
+    :param str page_data: raw page source data of the page
+    :return:  next page cursor 
+    :rtype: str
+    :exception: IndexError - if no next page  or any other error returns 0
+    :example: "/tag/steamprofile?cursor=MTQwYWI2MjA9NCY1OTBhY2FkMD03MiZkMTc0YjZiYz1OJTJGQQ"
+
+    | Made for selenuim version
+    | Accepts a deviant page source , searches for the next page cursor , and returns the cursor part 
+    | if no next page  or any other error returns 0
+
+
+    '''
+    try:
+        sleep(0.5)
+        pagedata = html.fromstring(page_data)
+        nextpage = pagedata.xpath('//*[text()="Next"]//@href')
+        return nextpage[0]
+    except IndexError:
+        return 0
+
+
+def remove_filter(steamurl: str) -> str:
     ''' 
-    :param steamurl: str - Steam url
-    :return: str - Steam url without filter tag
+    :param str steamurl:  Steam url
+    :return:  Steam url without filter tag
+    :rtype: str
 
     | Accepts a steam url and removes the filter part of the url and returns the url
     | Some urls have a filter part which is not needed for the program to work, this function removes the filter part and returns the url
-    
-    
+    | if no filter part is found returns the same url
 
     '''
     posifilter = steamurl.find('?filter')
