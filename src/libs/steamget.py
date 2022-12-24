e603b3051a185f3a1c7bb3a25c43f320412816c0
rakshith111 <rakshithbabu111@gmail.com>
2022-12-25 00:37:17 +0530

    Update steamget api

--- src/libs/steamget.py
+++ src/libs/steamget.py
@@ -4,43 +4,52 @@ import re
 import requests
 
 
-from urllib.parse import unquote
-
 # add proxy support
 
-def get_item(steamurl: str) -> str:  # type: ignore
+def get_item(steam_url: str, country_code=24) -> str:  # type: ignore
     """
-    :param str steamurl: Steam url 
+    :param str steam_url: Steam url 
+    :param int country_code: country code
     :rtype: str
     :example: get_item("https://steamcommunity.com/market/listings/753/746850-Chinatown" )
     :return: price of the item  returns ₹ 269.36  (price of the item)
 
+    :exception:  IndexError :  RE: Error in regex
+    :exception:  NP: No price found
+    :exception:  KeyError :  FA: Failed to get price
+
+
     | Takes a steammarket url and returns its price
+    | Optional country code, default is 24 for Indian-INR, 1 for US-USD
 
     """
-    patforid = (r"/[\d]+/")
-    patforname = (r"/[\d]+-[\w%()-'!$%&*+,-./:;<=>?@[\]^_`{|}~]+")
-    appid = re.findall(patforid, steamurl)
-    patforname = re.findall(patforname, steamurl)
+    try:
+        patforid = ("[\d]+/")
+        patforname = ("[\d]+-[\w%()'!$%&*+,-.:;<=>?@[\]^_`{|}~]+")
+        appid = re.findall(patforid, steam_url)
+        patforname = re.findall(patforname, steam_url)
+    except IndexError:
+        print("[-] Error in regex")
+        return "RE"
+    # print(f'[+] Got Appid for  Steamurl:{steam_url}  {appid[0].replace("/","")}')
+    # print(f'[+] Got Name for  Steamurl:{steam_url}  {patforname}')
 
-    url = f'https://steamcommunity.com/market/priceoverview/?country=US&currency=24&appid={appid[0].replace("/","")}&market_hash_name={patforname[0].replace("/","")}'
-    time.sleep(2)
+    url = f'https://steamcommunity.com/market/priceoverview/?currency={country_code}&appid={appid[0].replace("/","")}&market_hash_name={patforname[0].replace("/","")}'
+    time.sleep(0.5)
     resp = requests.get(url)
     try:
-        steamjsondata = json.loads(resp.content)
         if resp.ok:
             steamjsondata = json.loads(resp.content)
-
             if steamjsondata["lowest_price"]:  # if item exits  returns price
-                print(
-                    f'Got Price : { steamjsondata["lowest_price"]} Steamurl:{steamurl}  ')
+                print(f'[+] Got Price for  Steamurl:{steam_url}  {steamjsondata["lowest_price"]}')
                 return steamjsondata["lowest_price"]
             else:
-                return "NA"
+                return "NP"
         else:
+            print("[-] Possible timeout, trying again" )
             print(resp.url+" going to sleep for 5 sec ")
             print(steamjsondata)
             time.sleep(5)
-            get_item(steamurl)
+            get_item(steam_url)
     except KeyError:
-        return "NA"
+        return "FA"
