5b4ead4c199a0b2c9beb9d6e77590814c7999901
rakshith111 <rakshithbabu111@gmail.com>
2023-01-21 19:12:00 +0530

    Optimize loading and verifying Add function to create checkbox

--- src/libs/htmlgeny.py
+++ src/libs/htmlgeny.py
@@ -14,18 +14,40 @@ class htmlGeny:
         '''
         streamlit_jspath = pathlib.Path(
             st.__path__[0]) / 'static' / 'static' / 'js'
-        target_jspath = streamlit_jspath / 'table-sort.js'
-        source_file = os.path.join(os.path.dirname(
+
+        table_sortjs = os.path.join(os.path.dirname(
             os.path.dirname(__file__)), "scripts", "table-sort.js")
+        visitorjs = os.path.join(os.path.dirname(
+            os.path.dirname(__file__)), "scripts", "visitor.js")
+        target_jable_sortjs = streamlit_jspath / 'table-sort.js'
+        target_visitorjs = streamlit_jspath / 'visitor.js'
         self.css_path = os.path.join(os.path.dirname(
             os.path.dirname(__file__)), "scripts", "style.css")
 
-        if os.path.exists(target_jspath):
-            print('[+] table-sort.js exists')
-        else:
-            print('[x] table-sort.js does not exist')
-            shutil.copy(source_file, streamlit_jspath)
-            print('[+] table-sort.js copied')
+        files_to_check = [(table_sortjs, target_jable_sortjs),
+                          (visitorjs, target_visitorjs)]
+        for file_pair in files_to_check:
+            if os.path.exists(file_pair[1]):
+                print(f'[+] {file_pair[1]} exists')
+            else:
+                print(f'[x] {file_pair[1]} does not exist')
+                shutil.copy(file_pair[0], streamlit_jspath)
+                print(f'[+] {file_pair[1]} copied')
+
+    def addCheckbox(self, appid: str) -> str:
+        '''
+        :param str appid: Appid of the game
+        :rtype: str
+        :example: addCheckbox("123456)
+        :return: <input type="checkbox" data-id="123456">
+
+        | Takes a appid and returns a checkbox with data-id as the appid
+        | This is useful for adding a checkbox to a dataframe 
+        | then using the data-id to get the appid of the game for a persistent checkbox using local storage
+
+        '''
+
+        return f'<input type="checkbox" data-id="{appid}">'
 
     def make_clickable(self, link: str, market: bool = False) -> str:
         '''
@@ -76,14 +98,18 @@ class htmlGeny:
         to_outdata_steamdata['SteamUrl'] = steamdata['SteamUrl'].apply(
             self.make_clickable)
         to_outdata_steamdata["AppTag"] = steamdata['AppTag']
+        to_outdata_steamdata["Visited"] = to_outdata_steamdata["AppTag"].apply(
+            self.addCheckbox)
         to_outdata_steamdata["SteamPrice"] = steamdata['SteamPrice']
         to_outdata_steamdata["SteamPriceDate"] = steamdata['SteamPriceDate']
         to_outdata_steamdata["CardExchangeMarket"] = steamdata['AppTag'].apply(
             self.make_clickable, market=True)
         tablehtml = to_outdata_steamdata.to_html(
-            escape=False, index=False, classes="table-sort cont table-arrows ")
+            escape=False, index=False, classes="table-sort table-arrows visitor cool-theme")
         cssdata = open(f"{self.css_path}", 'r').read()
         tablehtml = tablehtml + f'<style>{cssdata}</style>'
+        with open('steamdata.html', 'w') as f:
+            f.write(tablehtml)
         st.write(tablehtml, unsafe_allow_html=True)
 
         st.title("Deviant Data")
@@ -97,7 +123,7 @@ class htmlGeny:
         to_out_deviantdata['SteamUrl'] = fulldata['SteamUrl'].apply(
             self.make_clickable)
         tablehtml = to_out_deviantdata.to_html(
-            escape=False, index=False, classes="table-sort cont  table-arrows ")
+            escape=False, index=False, classes="table-sort cool-theme  table-arrows ")
 
         st.write(tablehtml, unsafe_allow_html=True)
         st.title("No Steam Data")
@@ -107,12 +133,15 @@ class htmlGeny:
         to_out_deviantdata['SteamUrl'] = emptydata['SteamUrl'].apply(
             self.make_clickable)
         tablehtml = to_out_deviantdata.to_html(
-            escape=False, index=False, classes="table-sort cont  table-arrows ")
+            escape=False, index=False, classes="table-sort cool-theme table-arrows")
 
         st.write(tablehtml, unsafe_allow_html=True)
 
         html('''
         <script src='./static/js/table-sort.js'>
+        </script>''')
+        html('''
+        <script src='./static/js/visitor.js'>
         </script>''')
 
 
